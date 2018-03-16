e6d8881cd17ebf428bd4b3cc54426f1401de4cd0
Chris Johnston <Chris@thejohnstons.net>
2018-03-15 23:14:34 -0700

    rename from_binary to disassemble_instruction

--- easier68k/core/opcodes/opcode.py
+++ easier68k/core/opcodes/opcode.py
@@ -54,7 +54,7 @@ class Opcode:
         """
 
     @classmethod
-    def from_binary(cls, data: bytearray) -> (Opcode, int):
+    def disassemble_instruction(cls, data:bytearray) -> (str, int):
         """
         Parses some raw data into an instance of the opcode class
 
