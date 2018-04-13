61082ba688d25974c901337e114f18c06c3cf03d
Chris Johnston <chris@thejohnstons.net>
2018-04-13 03:26:42 -0700

    Add more tests for memory value

--- tests/easier68k/core/models/test_memory_value.py
+++ tests/easier68k/core/models/test_memory_value.py
@@ -3,6 +3,8 @@ Tests for core/models/memory_value
 which represent some value in memory with a length and a value
 """
 
+import pytest
+
 from easier68k.core.models.memory_value import MemoryValue
 from easier68k.core.enum.op_size import OpSize
 
@@ -40,3 +42,69 @@ def test_memory_value_cstr_and_eq():
     another_val = MemoryValue(OpSize.BYTE)
     assert (another_val.length == OpSize.BYTE)
     assert (another_val.unsigned_value == 0)
+
+def test_set_value_unsigned():
+    """
+    Tests the way that unsigned values are set and ensures that
+    values out of range for unsigned values cannot be set
+    :return:
+    """
+
+    val = MemoryValue(OpSize.LONG)
+    # try setting values that are good
+    val.set_value_unsigned_int(123)
+    assert (val.unsigned_value == 123)
+    val.set_value_unsigned_int(0xFFF)
+    assert (val.unsigned_value == 0xFFF)
+    val.set_value_unsigned_int(0xFFFFFFFF)
+    assert (val.unsigned_value == 0xFFFFFFF)
+
+    # try setting values that are out of bounds, they should all throw AssertException
+
+    # for len byte
+    val = MemoryValue(OpSize.BYTE)
+
+    # check that the max can be used correctly
+    val.set_value_unsigned_int(0xFF)
+    assert (val.unsigned_value == 0xFF)
+
+    # negative value
+    with pytest.raises(AssertionError):
+        val.set_value_unsigned_int(-1)
+    with pytest.raises(AssertionError):
+        val.set_value_unsigned_int(-12345)
+    # value that is too big for byte
+    with pytest.raises(AssertionError):
+        val.set_value_unsigned_int(0xFF + 1)
+
+    # for len word
+    val = MemoryValue(OpSize.WORD)
+
+    # check that the max can be used correctly
+    val.set_value_unsigned_int(0xFFFF)
+    assert (val.unsigned_value == 0xFFFF)
+
+    # negative value
+    with pytest.raises(AssertionError):
+        val.set_value_unsigned_int(-1)
+    with pytest.raises(AssertionError):
+        val.set_value_unsigned_int(-12345)
+    # value that is too big for word
+    with pytest.raises(AssertionError):
+        val.set_value_unsigned_int(0xFFFF + 1)
+
+    # for len long
+    val = MemoryValue(OpSize.LONG)
+
+    # check that the max can be used correctly
+    val.set_value_unsigned_int(0xFFFFFFFF)
+    assert (val.unsigned_value == 0xFFFFFFFF)
+
+    # negative value
+    with pytest.raises(AssertionError):
+        val.set_value_unsigned_int(-1)
+    with pytest.raises(AssertionError):
+        val.set_value_unsigned_int(-12345)
+    # value that is too big for word
+    with pytest.raises(AssertionError):
+        val.set_value_unsigned_int(0xFFFFFFFF + 1)
